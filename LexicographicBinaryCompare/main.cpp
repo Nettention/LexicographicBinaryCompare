6e2139a9814cf4209eb9d9f598013f3cd7a91f8d
Hyunjik Bae <imays76@gmail.com>
2019-07-30 01:42:23 +0900

    added more test cases.

--- LexicographicBinaryCompare/main.cpp
+++ LexicographicBinaryCompare/main.cpp
@@ -9,17 +9,17 @@ int main()
 
 	const char* p1 = "hello";
 	const char* p2 = "hello world";
-	
-	int v = LexicographicBinaryCompare32(p1, strlen(p1), p2, strlen(p2));
+
+	int v = LexicographicBinaryCompare32(p1, (int)strlen(p1), p2, (int)strlen(p2));
 	if (v < 0)
 		cout << "Test 1 ok.\n";
-	else 
+	else
 		cout << "Test 1 failed.\n";
 
 	p1 = "a hello";
 	p2 = "hello";
 
-	v = LexicographicBinaryCompare32(p1, strlen(p1), p2, strlen(p2));
+	v = LexicographicBinaryCompare32(p1, (int)strlen(p1), p2, (int)strlen(p2));
 	if (v < 0)
 		cout << "Test 2 ok.\n";
 	else
@@ -28,7 +28,7 @@ int main()
 	p1 = "abc123";
 	p2 = "abc133";
 
-	v = LexicographicBinaryCompare32(p1, strlen(p1), p2, strlen(p2));
+	v = LexicographicBinaryCompare32(p1, (int)strlen(p1), p2, (int)strlen(p2));
 	if (v < 0)
 		cout << "Test 3 ok.\n";
 	else
@@ -37,7 +37,7 @@ int main()
 	p1 = "abc123 ";
 	p2 = "abc133";
 
-	v = LexicographicBinaryCompare32(p1, strlen(p1), p2, strlen(p2));
+	v = LexicographicBinaryCompare32(p1, (int)strlen(p1), p2, (int)strlen(p2));
 	if (v < 0)
 		cout << "Test 4 ok.\n";
 	else
@@ -46,11 +46,20 @@ int main()
 	p1 = "abc123";
 	p2 = "abc123 ";
 
-	v = LexicographicBinaryCompare32(p1, strlen(p1), p2, strlen(p2));
+	v = LexicographicBinaryCompare32(p1, (int)strlen(p1), p2, (int)strlen(p2));
 	if (v < 0)
 		cout << "Test 5 ok.\n";
 	else
 		cout << "Test 5 failed.\n";
+
+	p1 = "abc123";
+	p2 = "abc123";
+
+	v = LexicographicBinaryCompare32(p1, (int)strlen(p1), p2, (int)strlen(p2));
+	if (v == 0)
+		cout << "Test 6 ok.\n";
+	else
+		cout << "Test 6 failed.\n";
 
 	std::cout << "Finished.\n";
 }
